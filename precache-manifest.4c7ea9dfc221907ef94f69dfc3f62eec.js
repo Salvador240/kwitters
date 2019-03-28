bd30e11f2fe6bca96cf832c861f3d202189822fd
Missi Perez <perezmissi@aol.com>
2019-03-28 16:06:16 -0400

    Updates

---
+++ precache-manifest.4c7ea9dfc221907ef94f69dfc3f62eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e1b16baec839270480e",
+    "url": "/kwitters/static/css/main.ba030707.chunk.css"
+  },
+  {
+    "revision": "2e1b16baec839270480e",
+    "url": "/kwitters/static/js/main.e406283f.chunk.js"
+  },
+  {
+    "revision": "df45802fcacdc278124c",
+    "url": "/kwitters/static/js/runtime~main.6114393e.js"
+  },
+  {
+    "revision": "ad26f102ccbfc9fc2913",
+    "url": "/kwitters/static/js/2.c3008c0d.chunk.js"
+  },
+  {
+    "revision": "34f3b9ef15e0669f103aea09a0b9b3fa",
+    "url": "/kwitters/static/media/favicon.34f3b9ef.ico"
+  },
+  {
+    "revision": "26d6e9e3633895dac79b469067654d72",
+    "url": "/kwitters/index.html"
+  }
+];
